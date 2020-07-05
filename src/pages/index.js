ba5c323cc8c2082e2407f7fd0bf45aed35ad6693
Jeongbong Seo <jngbng@gmail.com>
2020-07-06 00:13:52 +0900

    이미지 추가

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -8,14 +7,12 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Hi!</h1>
+    <p>걓!</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <a href="https://www.linkedin.com/in/youyoung-kim-12692892">Linkedin Page</a> <br />
   </Layout>
 )
 
